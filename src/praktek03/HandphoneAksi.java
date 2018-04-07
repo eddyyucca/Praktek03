4355a91fcb2ec98db551bf4bdfb631eacffeba1d
eddyyucca <eddyyucca@gmail.com>
2018-04-07 20:40:53 +0800

    instance buat atribut dan panggil method

--- src/praktek03/HandphoneAksi.java
+++ src/praktek03/HandphoneAksi.java
@@ -1,7 +1,12 @@
 package praktek03;
-
 public class HandphoneAksi {
     public static void main(String[] args) {
+        Handphone s = new Handphone();
+            s.merk="nokia";
+            s.warna="putih";
+            s.harga=1000000;
+            
+            s.cetakInfo();
         
     }
 }
